242d4fa499b7a32298073a4aab3bc3d26f70f92a
yourisrachid <yourisrachid@gmail.com>
2024-05-12 15:31:13 +0200

    filterworks

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,64 +1,65 @@
 export function filter() {
 
-    document.addEventListener('DOMContentLoaded', function() {    
+  document.addEventListener('DOMContentLoaded', function() {    
 
-        const allCheckbox = document.getElementById('all');             
-        allCheckbox.checked = true;                                       
-        filterBlocks();                                                   
-      
-      });
-      
-      const checkboxes = document.querySelectorAll('.category-checkbox');       
-      
-      //------ Event on checkbox.
-      
-      checkboxes.forEach(checkbox => {                                       
-        checkbox.addEventListener('change', function() {                      
-          uncheckFilter(checkbox);                                  
-        })
+      const allCheckbox = document.getElementById('all');             
+      allCheckbox.checked = true;                                       
+      filterBlocks();                                                   
+    
+    });
+    
+    const checkboxes = document.querySelectorAll('.category-checkbox');       
+    
+    //------ Event on checkbox.
+    
+    checkboxes.forEach(checkbox => {                                       
+      checkbox.addEventListener('change', function() {                      
+        uncheckFilter(checkbox);                                  
       })
-      
-      checkboxes.forEach(checkbox => {                                  
-        checkbox.addEventListener('change', function() {                     
-        filterBlocks();                                                 
-        });
+    })
+    
+    checkboxes.forEach(checkbox => {                                  
+      checkbox.addEventListener('change', function() {                     
+      filterBlocks();                                                 
       });
+    });
+    
+    //------ Filter the blocks
+    
+    
+    function filterBlocks() {
+      const checkedCategories = Array.from(document.querySelectorAll('.category-checkbox:checked')).map(checkbox => checkbox.id); 
+      const blocks = document.querySelectorAll('.block');
       
-      //------ Filter the blocks
-      
-      
-      function filterBlocks() {
-        NodeList.prototype.forEach = Array.prototype.forEach
-          const checkedCategories = Array.from(document.querySelectorAll('.category-checkbox:checked')).map(checkbox => checkbox.id); 
-          const blocks = Array.from(document.getElementsByClassName('.block'));                                                                            
-          blocks.forEach(block => {                                                                                                       
-            const category = block.getAttribute('id');                                                              
-            if (checkedCategories.includes(category) || checkedCategories.includes('all')) {                                            
-              blocks.style.display = 'flex';                                                                                             
-              const taskBlocks = document.querySelector('.taskBlocks');
-              taskBlocks.style.justifyContent = 'space-around';
-            } else {
-              blocks.style.display = 'none';                                            
-            }
-          });
-      }
-      
-      //------ Uncheck specific filters if ALL is checked and vice-versa.
-      
-      function uncheckFilter(checkbox) {
-        if (checkbox.id==='all' && checkbox.checked) {                                          
-      
-          checkboxes.forEach (box => {                                                
-      
-            if (box !== checkbox) {                                                          
-            }
-      
-          });
-        } else if (checkbox.id!== 'all' && checkbox.checked) {                                   
-            const uncheck = document.getElementById('all');                                 
-            uncheck.checked = false;                                                               
+      blocks.forEach(block => {                                                                                                       
+        const category = block.getAttribute('id');                                                              
+        if (checkedCategories.includes(category) || checkedCategories.includes('all')) {                                            
+          block.style.display = 'block';                                                         
+          const taskBlocks = document.querySelector('.taskBlocks');
+          taskBlocks.style.justifyContent = 'space-around';
+        } else {
+          block.style.display = 'none';
         }
+      });
+    }
+    
+    
+    //------ Uncheck specific filters if ALL is checked and vice-versa.
+    
+    function uncheckFilter(checkbox) {
+      if (checkbox.id==='all' && checkbox.checked) {                                          
+    
+        checkboxes.forEach (box => {                                                
+    
+          if (box !== checkbox) {                                                          
+          }
+    
+        });
+      } else if (checkbox.id!== 'all' && checkbox.checked) {                                   
+          const uncheck = document.getElementById('all');                                 
+          uncheck.checked = false;                                                               
       }
-      
+    }
+    
 
 }
